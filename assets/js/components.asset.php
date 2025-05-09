fb7044f30ff6697fe7064d569df19ab505038b70
Joy Sarkar <167693505+JoySarkar07@users.noreply.github.com>
2025-05-09 18:49:27 +0530

    Delete assets directory

--- assets/js/components.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '89626b0d93e8f7b79b09');
--- assets/js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '9a2e629d8bdd01df7a59');
